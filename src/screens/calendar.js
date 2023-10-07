a014a16e5572530e3cdb7b9673a27466de02822f
Bruce Quach <quachbruce@gmail.com>
2023-10-07 15:32:18 -0500

    Create calendar ratings

--- src/screens/calendar.js
+++ src/screens/calendar.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import { Calendar, Modal } from "antd";
+import React, { useEffect, useState } from "react";
+import { Calendar, Modal, Rate } from "antd";
 import styled from "styled-components";
 import AddIcon from "@mui/icons-material/AddCircle";
+import CircleIcon from "@mui/icons-material/Circle";
 
 const CalendarWrapper = styled.div`
   background-color: #daf0f7;
@@ -26,14 +27,37 @@ const TrackingCalendar = styled(Calendar)`
   border: 1px solid black;
   padding: 10px;
   border-radius: 10px;
+
+  td {
+    background-color: ${(props) =>
+      console.log(props)
+    }
+  }
 `;
+
+const customIcons = {
+  1: <CircleIcon style={{ color: "#FFCCCB" }} />,
+  2: <CircleIcon style={{ color: "#FFD580" }} />,
+  3: <CircleIcon style={{ color: "#FFDF00" }} />,
+  4: <CircleIcon style={{ color: "#9ACD32" }} />,
+  5: <CircleIcon style={{ color: "#008000" }} />,
+};
 
 const CalendarPage = () => {
   const [date, setDate] = useState(new Date());
   const [openModal, setOpenModal] = useState(false);
+  const [rating, setRating] = useState(0);
+
+  const [ratings, setRatings] = useState({});
+
+  let currentDate = date.toLocaleDateString();
+
+  useEffect(() => {
+    console.log(ratings);
+  });
 
   const renderCell = (value) => {
-    if (value.$d.toLocaleDateString() === date.toLocaleDateString()) {
+    if (value.$d.toLocaleDateString() === currentDate) {
       return (
         <div className="selected-date">
           <AddIcon className="add-icon" onClick={() => setOpenModal(true)} />
@@ -44,22 +68,41 @@ const CalendarPage = () => {
 
   const handleModalConfirm = () => {
     setOpenModal(false);
-  }
+  };
 
   return (
     <CalendarWrapper>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <span>Mental Health App</span>
         <span>
-          <strong>Currently Selected Date:</strong> {date.toLocaleDateString()}
+          <strong>Currently Selected Date:</strong> {currentDate}
         </span>
       </div>
       <TrackingCalendar
         onChange={(e) => setDate(e.$d)}
         onSelect={(e) => console.log(e)}
         cellRender={renderCell}
+        rating={rating}
       />
-      <Modal open={openModal} onCancel={() => setOpenModal(false)} onOk={handleModalConfirm}/>
+      <Modal
+        open={openModal}
+        onCancel={() => setOpenModal(false)}
+        onOk={handleModalConfirm}
+      >
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <span>Rate your Day!</span>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Rate
+              defaultValue={3}
+              character={({ index }) => customIcons[index + 1]}
+              onChange={(e) => {
+                setRating(e);
+                setRatings({ ...ratings, [currentDate]: e });
+              }}
+            />
+          </div>
+        </div>
+      </Modal>
     </CalendarWrapper>
   );
 };
