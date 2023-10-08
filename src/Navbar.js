cd8b07a5340d71efd3cae24cbdc8c3d971993399
Bruce Quach <quachbruce@gmail.com>
2023-10-08 15:25:44 -0500

    Make sliding landing page imgs

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,31 +1,36 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
-import './Navbar.css';
+import "./Navbar.css";
 
 export default function NavBar() {
-    return (
-        <nav className="nav">
-            <Link to="/" >
-                <p className="navbar_title">Psy-ki (气)</p>
-            </Link>
-            <ul>
-                <CustomLink className='navbar_calendar' to="/calendar">Calendar</CustomLink>
-                <CustomLink className='navbar_bot' to="/chatbot">Chat Bot</CustomLink>
-                <CustomLink className='navbar_profile' to="/overview">Profile</CustomLink>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <p className="navbar_title">Psy-気</p>
+      </Link>
+      <ul>
+        <CustomLink className="navbar_calendar" to="/calendar">
+          Calendar
+        </CustomLink>
+        <CustomLink className="navbar_bot" to="/chatbot">
+          Chat Bot
+        </CustomLink>
+        <CustomLink className="navbar_profile" to="/overview">
+          Profile
+        </CustomLink>
+      </ul>
+    </nav>
+  );
 }
 
 function CustomLink({ to, children, ...props }) {
-    const resolvedPath = useResolvedPath(to);
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
-    return (
-        <li className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>
-                {children}
-            </Link>
-        </li>
-    );
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
 }
-
--- src/screens/landing.js
+++ src/screens/landing.js
@@ -1,10 +1,56 @@
-import React from "react";
-import { Button } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Card } from "antd";
 import { useNavigate } from "react-router-dom";
 import Bread from "../images/bread.png";
+import Calendar from "../images/calendar.png";
+import Chatbot from "../images/chatbot.png";
+import Profile from "../images/profile.png";
+import "./landing.css";
 
 const LandingPage = () => {
   const navigate = useNavigate();
+
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const slides = [
+    {
+      cover: <img src={Bread} alt="Egg toast" style={{ height: 375 }} />,
+      content: "Presented by LSU Bread Boiz",
+    },
+    {
+      cover: <img src={Calendar} alt="Calendar page" style={{ height: 375 }} />,
+      content: "Make a calendar to log how your days are going.",
+    },
+    {
+      cover: <img src={Chatbot} alt="Chatbot page" style={{ height: 375 }} />,
+      content:
+        "Consult with a mental-health focused AI chatbot to reflect on your days.",
+    },
+    {
+      cover: <img src={Profile} alt="Profile page" style={{ height: 375 }} />,
+      content: "Keep a personal profile and goals to make your days better!",
+    },
+  ];
+
+  const nextSlide = () => {
+    setSlideNumber((prevSlide) => (prevSlide + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setSlideNumber(
+      (prevSlide) => (prevSlide - 1 + slides.length) % slides.length
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div
@@ -19,23 +65,54 @@ const LandingPage = () => {
     >
       <div
         style={{
-          border: "1px solid black",
-          borderRadius: 15,
+          display: "flex",
+          justifyContent: "center", // Center the cards horizontally
+          alignItems: "center", // Center the cards vertically
+        }}
+      >
+        {slides.map((slide, index) => (
+          <>
+            <Card
+              key={index}
+              cover={slide.cover}
+              style={{
+                position: "absolute",
+                // right: "25%",
+                top: "30%",
+                display: "flex",
+                margin: "0 auto",
+              }}
+              className={`card-content ${
+                index !== slideNumber ? "hidden" : ""
+              }`}
+            >
+              <p>{slide.content}</p>
+            </Card>
+          </>
+        ))}
+      </div>
+      <div
+        style={{
           width: "80%",
           padding: 20,
           height: "80%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          justifyContent: "center",
+          justifyContent: "flex-start",
         }}
       >
-        <h2>Psy-ki (气)</h2>
-        <img src={Bread} alt="Egg toast" width={200} />
-        <div>Project made by LSU Bread Boiz</div>
-        <div>Log daily feelings on how your day went</div>
-        <div>Get AI-generated feedback and ways to grow.</div>
-        <Button onClick={() => navigate("/calendar")}>Start your Day!</Button>
+        <h2>Psy-ki (気)</h2>
+        <div>
+          気: Japanese character that refers to one's spirit, mind, natural
+          energy, or health.
+        </div>
+        <Button
+          style={{ marginTop: "1%" }}
+          onClick={() => navigate("/calendar")}
+        >
+          Start your Day!
+        </Button>
       </div>
     </div>
   );
